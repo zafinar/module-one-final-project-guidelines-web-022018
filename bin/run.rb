056a5b291ecf2e6fd1f265ea1f740fe3c3b9d91a
zafinar <zafinar@gmail.com>
2018-03-08 21:36:24 -0500

    Done

--- bin/run.rb
+++ bin/run.rb
@@ -1,34 +1,46 @@
 require_relative '../config/environment.rb'
 require 'pry'
 api = Api.new
-address = api.address
-complaint = api.complaint
+# address = api.address
+# complaint = api.complaint
+join = api.join
 
-def create_complaint_table(complaint)
-  complaint.each {|i| Complaint.create(i)}
-end
+# def create_complaint_table(complaint)
+#   complaint.each {|i| Complaint.create(i)}
+# end
+#
+# def create_address_table(address)
+#   address.each {|i| Address.create(i)}
+# end
+#try debugging this BITCHES
+def create_join_table(join)
+  join.each do |i|
+    c = Complaint.create(i.except(:incident_address,:incident_zip,:cross_street_1,:cross_street_2,:borough,:city,:location_type,:address_type,:street_name)).find_or_create_by(:complaint_type, i[:complaint_type])
+    a = Address.create(i.except(:complaint_type,:descriptor)).find_or_create_by(:incident_address, i[:incident_address])
+    # ComplaintAddress.create(address_id: a.id, complaint_id: c.id)
 
-def create_address_table(address)
-  address.each {|i| Address.create(i)}
-end
-
-def create_join_table
-  idx = 0
-  while idx < Complaint.all.length
-    ComplaintAddress.create({address_id: Address.all[idx].id, complaint_id: Complaint.all[idx].id})
-      idx +=1
+binding.pry
   end
+
 end
+
+# def create_join_table
+#   idx = 0
+#   while idx < Complaint.all.length
+#     ComplaintAddress.create({address_id: Address.all[idx].id, complaint_id: Complaint.all[idx].id})
+#       idx +=1
+#   end
+# end
 
 # create_join_table(address,complaint)
 # create_address_table(address)
 # create_complaint_table(complaint)
-create_join_table
+create_join_table(join)
 
 # ct = ComplaintType.create(name: respose_hash.name, etc)
 #   ia = IncidentAddress.create(address: response_hash.address, etc)
 #   ComplaintAddress.create(complaint_type_id: ct.id, incident_address_id: ia.id)
 
 
-binding.pry
+# binding.pry
 puts 'lol'
--- lib/api.rb
+++ lib/api.rb
@@ -9,32 +9,51 @@ class Api
         # binding.pry
       end
 
-      def complaint
+      # def complaint
         # complaint = {complaint_type: "",descriptor: ""}
-        @call.map do |i|
+        # @call.map do |i|
           # if i.key?("incident_address")
-            complaint = {
+            # complaint = {
+            # complaint_type: i["complaint_type"],
+            # descriptor: i["descriptor"]
+          # }
+      #     end
+      #   end
+      # end
+      #
+      # def address
+      #   @call.map do |i|
+          # address = {
+          #    incident_address: i["incident_address"],
+          #    incident_zip: i["incident_zip"],
+          #    cross_street_1: i["cross_street_1"],
+          #    cross_street_2: i["cross_street_2"],
+          #    borough: i["borough"],
+          #    city: i["city"],
+          #    location_type: i["location_type"],
+          #    address_type: i["address_type"],
+          #    street_name: i["street_name"]
+          #
+          #   }
+      #     end
+      # end
+      def join
+        @call.map do |i|
+          join ={
+            incident_address: i["incident_address"],
+            incident_zip: i["incident_zip"],
+            cross_street_1: i["cross_street_1"],
+            cross_street_2: i["cross_street_2"],
+            borough: i["borough"],
+            city: i["city"],
+            location_type: i["location_type"],
+            address_type: i["address_type"],
+            street_name: i["street_name"],
             complaint_type: i["complaint_type"],
             descriptor: i["descriptor"]
-          }
-          end
-      
-      end
-
-      def address
-        @call.map do |i|
-          address = {
-             incident_address: i["incident_address"],
-             incident_zip: i["incident_zip"],
-             cross_street_1: i["cross_street_1"],
-             cross_street_2: i["cross_street_2"],
-             borough: i["borough"],
-             city: i["city"],
-             location_type: i["location_type"],
-             address_type: i["address_type"],
-             street_name: i["street_name"]
             }
-          end
+
+        end
       end
 end
 # api = Api.new.call
